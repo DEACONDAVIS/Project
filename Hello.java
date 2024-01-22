80df6c8071569ce2f03a222849d765b52e906e01
DEACONDAVIS <142839767+DEACONDAVIS@users.noreply.github.com>
2024-01-22 10:03:15 -0700

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        String myName = "Deacon Davis";
+        int myAge =15;
+        String momName = "Kodie Davis";
+        String dadName = "Christopher Davis";
+        String broName = "Parker Davis";
+        String sisName = "McKay Davis";
+
+        System.out.println("Hello World");
+        System.out.println("My name is " + myName + " and my age is " + myAge);
+        System.out.println("My parents names are " + momName + " and " + dadName);
+        System.out.println("My siblings names are " + broName + " and " + sisName);
+    }
+}
