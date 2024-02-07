45652e49e6f62ab026dc3caa0b72eaebab51f8d7
DEACONDAVIS <142839767+DEACONDAVIS@users.noreply.github.com>
2024-02-07 09:54:55 -0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,25 +1,37 @@
 public class Test {
 public static void main(String[] args)
 {
-// here is my interger for the final sum of all the numbers, as you see at the end of my loop I add the zariable i to finalSum and then I print the sum at the end of the loop
-    int finalSum = 0;
-// here I use an for loop with the initializing statement that int i = 0.
-// my conditional statement which is checked each time the code loops is i < 100
-// the i++ at the end of my for loop means that at the end of each loop the interger adds one            SYNTAX: being i used to equal 0 but after the first loop it equals 1
-    for (int i = 0; i <= 100; i++)
+// this variable will add all of the numbers in my index together to make a runnning total.
+int finalSum = 0;
+
+
+int[] nmbrs = new int[100];
+// the above line of code creates my array which is named nmbrs and it has an index of 100 
+// the below line of code makes a loop that adds a value to an index in my array each time it loops. the values count up to one hundred.
+for(int i=0; i<100; i++)
+{
+// this below line of code tells my code that all the values in the array nmbrs are equal to the value of the current index plus 1
+   nmbrs[i] = i+1; 
+}
+// this for loop states that the code in brackets should be done while i is less than the length of the array nmbrs
+for(int i=0; i<nmbrs.length; i++)
+{
+    finalSum = finalSum + nmbrs[i];
+    System.out.print(nmbrs[i]);
+// the lines 22-30 tell my code that if you divide the value of the current index by two and it has a remainder, than it is an odd number.
+    if(nmbrs[i] % 2 == 0)
     {
-        System.out.print(i);
-        
-        if (i % 2 == 0){
-        System.out.println(" Even");
-        }
-        else{
-        System.out.println(" Odd");
-        }
-        finalSum = finalSum + i;
+    System.out.println(" Even");
     }
-    System.out.println(finalSum);
-
-
+    else
+    {
+    System.out.println(" Odd");
+    }
+}
+// this final line of code prints out the running total of all the numbers in the array nmbrs
+System.out.println(finalSum);
 }
 }
+
+
+
